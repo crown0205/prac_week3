3d603c8b4e56bab7f6450aea8deffe87adb61ebf
crown0205 <crown0205@gmail.com>
2022-04-01 14:10:55 +0900

    1-10 Gird 컴포넌트 만들어 스타일 만든후 App.js에 적용해주기

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,26 +1,30 @@
 import React from "react";
-
-const Post = (props)  =>{
-
+import Grid from "../elements/Gird";
+const Post = props => {
   return (
     <React.Fragment>
-      <div>user profile/ user name / insert_dt / is_me (edit btn)</div>
-      <div>contents</div>
-      <div>image</div>
-      <div>comment cnt</div>
+      {/* 그리드, 텍스트, 버튼, 이미지 컴포넌트 만들꺼임 */}
+      <Grid padding="16px">
+        <div>user profile/ user name / insert_dt / </div>
+        <div>contents</div>
+        <div>image</div>
+        <div>comment cnt</div>
+      </Grid>
     </React.Fragment>
-  )
-}
+  );
+};
 
 Post.defaultProps = {
   user_info: {
     user_name: "bingi",
-    user_profile: "https://i.pinimg.com/564x/a2/fa/50/a2fa50e5c6eb5e3324c90b7f044c597e.jpg"
+    user_profile:
+      "https://i.pinimg.com/564x/a2/fa/50/a2fa50e5c6eb5e3324c90b7f044c597e.jpg",
   },
-  image_url: "https://i.pinimg.com/564x/a2/fa/50/a2fa50e5c6eb5e3324c90b7f044c597e.jpg",
+  image_url:
+    "https://i.pinimg.com/564x/a2/fa/50/a2fa50e5c6eb5e3324c90b7f044c597e.jpg",
   contents: "사람이오",
   comment_cnt: 10,
-  insert_dt: "2022-03-31 12:14:32"
-}
+  insert_dt: "2022-03-31 12:14:32",
+};
 
 export default Post;
