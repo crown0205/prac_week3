825e05b4a174eff55c4a31c23573bcc8a094c574
crown0205 <crown0205@gmail.com>
2022-04-03 22:12:17 +0900

    2-15 게시글 추가 버튼 만들기 스타일

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -2,24 +2,36 @@ import React from "react";
 import styled from "styled-components";
 
 const Button = props => {
-
-  const { fontW,width, height, margin ,bg, color, border } = props;
+  const { fontW, width, height, margin, bg, color, border, is_float } = props;
 
   const styles = {
     width: width,
     height: height,
     margin: margin,
     fontW: fontW,
-    bg:bg,
-    color:color,
-    border:border,
+    bg: bg,
+    color: color,
+    border: border,
   };
 
-  return <ButtonBox {...styles} onClick={props._onClick}>{props.text}</ButtonBox>;
+  if (is_float) {
+    return (
+      <React.Fragment>
+        <FloatButton onClick={props._onClick}>{props.text}</FloatButton>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <ButtonBox {...styles} onClick={props._onClick}>
+      {props.text}
+    </ButtonBox>
+  );
 };
 
 Button.defaultProps = {
   children: null,
+  is_float: false,
   width: "40%",
   height: "30px",
 };
@@ -31,8 +43,24 @@ const ButtonBox = styled.button`
   font-weight: ${props => props.fontW};
   ${props => (props.bg ? `background: ${props.bg};` : "")}
   ${props => (props.margin ? `margin: ${props.margin};` : "")}
-  /* border:none; */
   ${props => (props.border ? `border: ${props.border};` : "")}
+`;
+
+const FloatButton = styled.button`
+  width: 50px;
+  height: 50px;
+  background-color: #212121;
+  color: #fff;
+  box-sizing: border-box;
+  font-size: 36px;
+  font-weight: 800;
+  position: fixed;
+  bottom: 50px;
+  right: 50px;
+  text-align: center;
+  vertical-align: middle;
+  border: none;
+  border-radius: 50%;
 `;
 
 export default Button;
