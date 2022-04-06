2cbd6b29ae4651c314e8a62829ab158e25400fcd
crown0205 <crown0205@gmail.com>
2022-04-07 00:06:33 +0900

    db에 코멘트 데이터 불러와 detail page에 보여주기 완료!

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,26 +1,48 @@
 import React from "react";
 import { Grid, Image, Text } from "../elements";
 
-const CommentList = () => {
-  return(
+import { useDispatch, useSelector } from "react-redux";
+import { actionCreators as commentActions } from "../redux/modules/comment";
+
+const CommentList = props => {
+  const dispatch = useDispatch();
+  const comment_list = useSelector(state => state.comment.list)
+  const { post_id } = props;
+
+  React.useEffect(() => {
+
+    if(!comment_list[post_id]) {
+      dispatch(commentActions.getCommentFB(post_id))
+    }
+  },[])
+
+  if(!comment_list[post_id] || !post_id) {
+    return null;
+  }
+
+  return (
     <React.Fragment>
-      <Grid>
-        <CommentItem/>
-        <CommentItem/>
-        <CommentItem/>
-        <CommentItem/>
+      <Grid padding="16px">
+        {comment_list[post_id].map( item => {
+          return (<CommentItem key={item.id} {...item} />)
+        } )}
       </Grid>
     </React.Fragment>
-  )
+  );
+};
+
+CommentList.defaultProps = {
+  post_id:null,
+
 }
 
 const CommentItem = props => {
-
-  const { user_profile, user_name, user_id, post_id, contents, insert_dt } = props;
+  const { user_profile, user_name, user_id, post_id, contents, insert_dt } =
+    props;
   return (
     <Grid is_flex padding="0 16px">
       <Grid is_flex width="auto">
-        <Image shape="circle"/>
+        <Image shape="circle" />
         <Text bold>aaa{user_name}</Text>
       </Grid>
       <Grid is_flex margin="0 4px">
@@ -29,7 +51,7 @@ const CommentItem = props => {
       </Grid>
     </Grid>
   );
-}
+};
 
 CommentItem.defaultProps = {
   user_profile: "",
@@ -37,7 +59,7 @@ CommentItem.defaultProps = {
   user_id: "",
   post_id: 1,
   contents: "귀여운 고양이네요!",
-  insert_dt: '2021-01-01 19:00:00'
-}
+  insert_dt: "2021-01-01 19:00:00",
+};
 
 export default CommentList;
